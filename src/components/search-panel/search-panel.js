d648c01285720eee74b4d7f6ceeb6c594f6b0141
Gabriel <gabiplacinta77@gmail.com>
2023-02-07 13:19:40 +0200

    first commit

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,9 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <input type="text" className="form-control search-input" placeholder="Search for employee"/>
+    )
+}
+
+export default SearchPanel;
